a5786f826350d6a479b2cc02c47e21b3cad35606
Yadnesh Naik <naikyadnesh9@gmail.com>
2025-12-11 13:50:57 +0530

    init

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -5,23 +5,12 @@ export default function NavbarComp() {
         Vista Developer
       </a>
 
-      <div className="collapse navbar-collapse show" id="navBar">
+     <div className="collapse navbar-collapse" id="navBar">
         <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="/">Home</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="/services">Services</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="/about">About</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="/contact">Contact</a>
-          </li>
+          <li className="nav-item"><a className="nav-link purple-link" href="/">Home</a></li>
+          <li className="nav-item"><a className="nav-link purple-link" href="/services">Services</a></li>
+          <li className="nav-item"><a className="nav-link purple-link" href="/about">About</a></li>
+          <li className="nav-item"><a className="nav-link purple-link" href="/contact">Contact</a></li>
         </ul>
       </div>
     </nav>
