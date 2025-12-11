dc65885023ec659c58243d5c22603f215d9dd711
Yadnesh Naik <naikyadnesh9@gmail.com>
2025-12-11 13:49:27 +0530

    init

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,22 +1,27 @@
 export default function NavbarComp() {
   return (
-    <nav className="navbar navbar-expand-lg px-3 transparent-nav">
-      <a className="navbar-brand fw-bold text-dark">Vista Developer</a>
+    <nav className="navbar navbar-expand-lg px-3 bg-transparent">
+      <a className="navbar-brand fw-bold text-dark" href="/">
+        Vista Developer
+      </a>
 
-      <button 
-        className="navbar-toggler" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navBar"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navBar">
+      <div className="collapse navbar-collapse show" id="navBar">
         <ul className="navbar-nav ms-auto">
-          <li className="nav-item"><a className="nav-link purple-link" href="/">Home</a></li>
-          <li className="nav-item"><a className="nav-link purple-link" href="/services">Services</a></li>
-          <li className="nav-item"><a className="nav-link purple-link" href="/about">About</a></li>
-          <li className="nav-item"><a className="nav-link purple-link" href="/contact">Contact</a></li>
+          <li className="nav-item">
+            <a className="nav-link text-dark" href="/">Home</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link text-dark" href="/services">Services</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link text-dark" href="/about">About</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link text-dark" href="/contact">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
